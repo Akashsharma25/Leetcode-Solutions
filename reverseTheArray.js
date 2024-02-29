71974e49ced9d978b718305fe6913a91887e9eba
akash <akasharma2525@gmail.com>
2024-02-29 15:13:46 +0530

    adding solution

---
+++ reverseTheArray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s =  s.reverse();
+    
+};
+// function reverseString(s) {
+//     let left = 0;
+//     let right = s.length - 1;
+
+//     while (left < right) {
+//         // Swap characters at left and right pointers
+//         [s[left], s[right]] = [s[right], s[left]];
+
+//         // Move pointers towards the center
+//         left++;
+//         right--;
+//     }
+// }
